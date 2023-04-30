337f31f6f1d7f0717d17528224543bb06bc303e8
Ayobami <ayobamidele006@gmail.com>
2023-04-30 11:49:04 +0100

    add staffs, student profile and fix db relations

--- sms_backend/staffs/models.py
+++ sms_backend/staffs/models.py
@@ -1,40 +1,29 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 # Create your models here.
 
-
-class Staffs(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    phone = models.CharField(max_length=20)
-    address = models.CharField(max_length=100)
-    is_admin = models.BooleanField(default=False)
-    salary = models.IntegerField()
-    photo = models.ImageField(upload_to='staffs', null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return '{}-{}'.format(self.id, self.name)
-
 # create staff custom user model
+
+# staff user profile
 
 
 class StaffUser(AbstractUser):
-    is_staff = models.BooleanField(default=True)
+    groups = models.ManyToManyField(Group, related_name='staff_users')
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='staff_users',
+        blank=True,
+    )
 
-# create staff profile model
 
-
-class StaffProfile(models.Model):
+class Staffs(models.Model):
     staff_user = models.OneToOneField(
-        StaffUser, on_delete=models.CASCADE, primary_key=True)
+        StaffUser, on_delete=models.CASCADE, default=None)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     phone = models.CharField(max_length=20)
     address = models.CharField(max_length=100)
-    is_admin = models.BooleanField(default=False)
     salary = models.IntegerField()
     photo = models.ImageField(upload_to='staffs', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- sms_backend/students/admin.py
+++ sms_backend/students/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import StudentUser, Student
 
 # Register your models here.
+admin.site.register(StudentUser)
+admin.site.register(Student)
